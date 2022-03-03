10324a1f9114db58dda05d23990fd1356f0809c3
javi-dh <javier@digitalhouse.com>
2022-03-02 21:18:40 -0500

    Clase 37 - JS Front - Manipulando Elementos

---
+++ Mod-08/clase-37-manipulando-elementos-js/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const methodOverride = require("method-override");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(express.static(path.resolve(__dirname, "public")));
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`App running in port: ${PORT}`));
+
+// Template Engine
+app.set("view engine", "ejs");
+
+// Routers
+const productsRouter = require("./routes/products");
+app.use("/products", productsRouter);
+
+app.get("/", (req, res) => res.redirect("/products"));
